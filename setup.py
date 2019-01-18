c4f421776e9080e6ccf32389f5a561d118816af2
Joseph Jung <joseph@hologram.io>
2019-01-18 12:18:27 -0600

    Add python requires in setup.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     url="https://magic.co",
     packages=find_packages(),
     package_data={'magic': ['resources/*']},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 
     entry_points={
         'console_scripts': [
